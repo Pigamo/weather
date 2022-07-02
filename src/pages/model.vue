71ff44f5a7fd49fb4b074f3f81f5c6a67ebcfed6
Pigamo <chosongames@gmail.com>
2022-07-02 15:33:55 -0400

    Added Model for real this time

---
+++ src/pages/model.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Use it like any other HTML element -->
+  <model-viewer
+    class="w-screen h-screen"
+    alt="Modem"
+    src="./modem.glb"
+    ar
+    ar-modes="webxr scene-viewer quick-look"
+    skybox-image="./lebombo_4k.hdr"
+    field-of-view="5deg"
+    camera-orbit="110deg 70deg "
+    shadow-intensity="1"
+    camera-controls
+    enable-pan
+  ></model-viewer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
